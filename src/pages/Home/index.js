a99cb315ef6fd6f0bcc07e4cdfb7d2f9ac6d980e
salsabilaathika-97 <salsathikar@gmail.com>
2022-09-15 21:29:24 +0800

    add description for product and home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,12 @@
+import {Helmet} from "react-helmet-async"
+
 const Home = () => {
     return (
         <div>
+            <Helmet>
+                <title>Home</title>
+                <meta name="description" content="Halaman ini merupakan halaman utama yang ditampilkan pertama kali"></meta>
+            </Helmet>
             <h1>Halaman Home</h1>
         </div>
     )
--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,6 +1,12 @@
+import {Helmet} from "react-helmet-async"
+
 const Products = () => {
     return (
         <div>
+            <Helmet>
+                <title>Home</title>
+                <meta name="description" content="Halaman ini merupakan hasil produk dari projek yang telah dikerjakan"></meta>
+            </Helmet>
             <h1>Halaman Products</h1>
         </div>
     )
