bb0f6051eebb2ad0892cd135b09545e050e540a4
salsabilaathika-97 <salsathikar@gmail.com>
2022-09-15 21:23:33 +0800

    add description for about page

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,6 +1,12 @@
+import {Helmet} from "react-helmet-async"
+
 const About = () => {
     return (
         <div>
+            <Helmet>
+                <title>About</title>
+                <meta name="description" content="Halaman ini menampilkan rincian projek yang dikerjakan"></meta>
+            </Helmet>
             <h1>Halaman About</h1>
         </div>
     )
